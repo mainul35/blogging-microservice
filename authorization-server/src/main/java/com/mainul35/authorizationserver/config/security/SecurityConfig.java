9e55f2a51333d943e8791d57ebc6d1eb6a0e4081
mainul35 <mainuls18@gmail.com>
2023-12-14 23:06:53 +0700

    blogging authorization server - token customization

--- authorization-server/src/main/java/com/mainul35/authorizationserver/config/security/SecurityConfig.java
+++ authorization-server/src/main/java/com/mainul35/authorizationserver/config/security/SecurityConfig.java
@@ -88,6 +88,22 @@ public class SecurityConfig {
     }
 
     @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer () {
+        return context -> {
+            Authentication principal = context.getPrincipal();
+            if (context.getTokenType().getValue().equals("id_token")) {
+                context.getClaims().claim("token_type", "id token");
+            }
+
+            if (context.getTokenType().getValue().equals("access_token")) {
+                context.getClaims().claim("token_type", "access token");
+                Set<String> roles = principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+                context.getClaims().claim("roles", roles).claim("username", principal.getName());
+            }
+        };
+    }
+
+    @Bean
     public ClientSettings clientSettings(){
         return ClientSettings.builder().requireAuthorizationConsent(true).requireProofKey(true).build();
     }
