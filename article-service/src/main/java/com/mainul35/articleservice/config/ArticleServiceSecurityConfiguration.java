2d0a042384bad66f00a80dc5b7c99ad0fc30798c
mainul35 <mainuls18@gmail.com>
2023-12-18 07:32:48 +0700

    resource server - security configuration changes

--- article-service/src/main/java/com/mainul35/articleservice/config/ArticleServiceSecurityConfiguration.java
+++ article-service/src/main/java/com/mainul35/articleservice/config/ArticleServiceSecurityConfiguration.java
@@ -1,10 +1,14 @@
 package com.mainul35.articleservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -14,21 +18,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration(proxyBeanMethods = false)
 public class ArticleServiceSecurityConfiguration {
 
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	private String issuerUri;
+
 	// @formatter:off
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.securityMatcher("/articles/**");
-		http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
-			authorizationManagerRequestMatcherRegistry
-					.requestMatchers("/articles/**").hasAuthority("SCOPE_message.read");
-
-				})
-				.oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
-					httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
-				});
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+				.oauth2ResourceServer(oauth -> oauth.jwt(jwt ->
+						jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))
+				));
 		return http.build();
 	}
 	// @formatter:on
+
+	@Bean
+	public JwtAuthenticationConverter jwtAuthenticationConverter () {
+		JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+		converter.setAuthoritiesClaimName("roles");
+		converter.setAuthorityPrefix("");
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+		return jwtAuthenticationConverter;
+	}
 
 }
