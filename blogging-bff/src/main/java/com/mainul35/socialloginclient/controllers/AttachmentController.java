10d986db2cefd49aa2140ed8e1bbc5010481d0a8
mainul35 <mainuls18@gmail.com>
2024-02-22 01:09:01 +0700

    Adding @PreAuthorize for roles

--- blogging-bff/src/main/java/com/mainul35/socialloginclient/controllers/AttachmentController.java
+++ blogging-bff/src/main/java/com/mainul35/socialloginclient/controllers/AttachmentController.java
@@ -1,6 +1,7 @@
 package com.mainul35.socialloginclient.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class AttachmentController {
 
     @PostMapping("/upload")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER', 'OIDC_USER')")
     public ResponseEntity<String> saveAttachmentAndReturnUrl (@RequestParam(value="file") MultipartFile file, @RequestParam(value="articleTempId") String articleTempId) {
         System.out.println(articleTempId);
         return ResponseEntity.ok("");
