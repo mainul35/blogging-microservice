1cbb7cc3e6bff93c03eb756b0ade633b7712246f
mainul35 <mainuls18@gmail.com>
2024-05-30 08:33:51 +0700

    removing unnecessary codes

--- authorization-server/src/main/java/com/mainul35/authorizationserver/config/security/fedarated/FederatedIdentityConfigurer.java
+++ authorization-server/src/main/java/com/mainul35/authorizationserver/config/security/fedarated/FederatedIdentityConfigurer.java
@@ -1,9 +1,9 @@
 package com.mainul35.authorizationserver.config.security.fedarated;
 
+import com.mainul35.authorizationserver.repository.ClientRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.util.Assert;
@@ -68,10 +68,8 @@ public final class FederatedIdentityConfigurer extends AbstractHttpConfigurer<Fe
     @Override
     public void init(HttpSecurity http) throws Exception {
         ApplicationContext applicationContext = http.getSharedObject(ApplicationContext.class);
-        ClientRegistrationRepository clientRegistrationRepository =
-                applicationContext.getBean(ClientRegistrationRepository.class);
         FederatedIdentityAuthenticationEntryPoint authenticationEntryPoint =
-                new FederatedIdentityAuthenticationEntryPoint(this.loginPageUrl, clientRegistrationRepository);
+                new FederatedIdentityAuthenticationEntryPoint(this.loginPageUrl);
         if (this.authorizationRequestUri != null) {
             authenticationEntryPoint.setAuthorizationRequestUri(this.authorizationRequestUri);
         }
