44748934d0b63fa16a8968a4be58c5d06f6fea6d
syedmainul.hasan <syedmainul.hasan@rakuten.com>
2025-07-09 17:14:08 +0900

    clean up unnecessary classes

--- authorization-server/src/main/java/com/mainul35/authorizationserver/repository/Oauth2Authorization.java
+++
@@ -1,6 +0,0 @@
-package com.mainul35.authorizationserver.repository;
-
-import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
-
-public class Oauth2Authorization extends OAuth2Authorization {
-}
